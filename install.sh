6d56e99373bd3a5f0ae816196dc506401fac9b84
slipsknuten <gurra.bergqvist@gmail.com>
2025-08-01 21:17:16 +0200

    add package lists and update install script

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
 sudo pacman -S --needed - < pacman-packages.txt
-
 if ! command -v yay &> /dev/null; then
     git clone https://aur.archlinux.org/yay.git
     cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay
 fi
-
 yay -S --needed --noconfirm - < packages.txt
-
-stow hypr
+stow hypr waybar nvim
