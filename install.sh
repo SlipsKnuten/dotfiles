c45bb80497d14740e4d7dca70c5804969ee1e849
slipsknuten <gurra.bergqvist@gmail.com>
2025-08-03 01:33:21 +0200

    add tmux and ghostty configurations

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ if ! command -v yay &> /dev/null; then
     cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay
 fi
 yay -S --needed --noconfirm - < packages.txt
+sudo sed -i 's/#sv_SE.UTF-8 UTF-8/sv_SE.UTF-8 UTF-8/' /etc/locale.gen
+sudo locale-gen
+sudo localectl set-locale LANG=sv_SE.UTF-8
 stow hypr waybar nvim zsh tmux ghostty
-
